4156b2bb2e31ee9af0f125be25c7e94aabc1034e
asidermakriver <asidermakriver@gmail.com>
2021-09-19 02:39:38 +0300

    Add JS and CSS

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
-// toLocaleDateString
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+//document.querySelector("#lastupdated").textContent = document.lastModified();
+//console.log(lastupdated);
+
+let lastupdated = document.lastModified;
+document.querySelector("#lastupdated").textContent = lastupdated;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* toLocaleDateString
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
-document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);*/
