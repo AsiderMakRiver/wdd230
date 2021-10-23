7add51b796d7695bf482016ed30694c593f0dd52
asidermakriver <asidermakriver@gmail.com>
2021-10-24 01:10:53 +0300

    lesson 6 addings

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,2 +1,14 @@
-f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);
+//! f is the wind chill factor in Fahrenheit
+//! t is the air average temperature in Fahrenheit
+//! s is the wind speed in miles per hour.
+
+function computeWindChill() {
+  //Get two inputs: t and s.
+  //Compute Wind Chill by this formula below.
+  let t = document.getElementById("temperature").value;
+  let s = document.getElementById("windSpeed").value;
+  let f = 35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16);\
+  console.log(f);
+}
+document.getElementById("output").innerHTML = f;
 console.log(f);
