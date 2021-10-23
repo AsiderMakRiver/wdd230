002cbf9e6de42df3e665d5d7db02df51c2477982
asidermakriver <asidermakriver@gmail.com>
2021-10-24 00:00:41 +0300

    Lesson 6 added

--- lesson6/js/script.js
+++ lesson6/js/script.js
@@ -18,6 +18,5 @@ const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll("nav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
     link.classList.add("active");
-    console.log(link);
   }
 });
