26247a8c548bc4e81e2d904d5f9e51805e25b61a
asidermakriver <asidermakriver@gmail.com>
2021-11-18 17:20:24 +0300

    Fix L7

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -1,4 +1,4 @@
-//TODO The time
+//**TODO:****************  The time **********
 const a = new Date();
 const options = {
   weekday: "long",
@@ -7,13 +7,12 @@ const options = {
   year: "numeric",
 };
 document.getElementById("lastupdated").textContent = a.toLocaleDateString("en-US", options);
-
-//TODO The nav hamburger script
+//TODO: **          The nav hamburger script
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
 }
 
-//TODO: Active link
+//TODO: **             Active link
 const activePage = window.location.pathname;
 const navLinks = document.querySelectorAll("nav a").forEach((link) => {
   if (link.href.includes(`${activePage}`)) {
@@ -21,12 +20,29 @@ const navLinks = document.querySelectorAll("nav a").forEach((link) => {
   }
 });
 
-//?********Local Storage
+//TODO: **            Local Storage
+const millisecondsToday = new Date().getTime(); //Milliseconds today from 1 january 1970
+localStorage.setItem("millisecondsTime", millisecondsToday); //Put that information to Local Storage
+const box1 = localStorage.getItem("milisecondsTime", millisecondsToday); //Get information from Local Storage to JS
+const millisecondsOneDay = 86400000;
+localStorage.setItem("millisecondsOneDay", 86400000);
+const box2 = millisecondsToday;
+console.log(box1);
+console.log(box2);
+// localStorage.setItem("person", JSON.stringify(object));
+// console.log(localStorage.setItem("person", JSON.stringify(object)));
 
-localStorage.setItem("name", "bob");
-localStorage.setItem("flex", "bob3");
-localStorage.setItem("flex", "bob7");
-console.log(localStorage.getItem("flex"));
-localStorage.removeItem("name");
-localStorage.removeItem("flex");
-localStorage.setItem("huba", "5 + 5");
+// const raw = localStorage.getItem("person");
+// const person = JSON.parse(raw);
+// person.name = "Vladilen";
+// console.log(person);
+
+//!:**                    A hint
+//?** determine days between stored date and visit date... today
+//?** determine today date
+const millisecondToDays = 86400000; //? divede the difference between dates un milli
+//?** Last visit ... stored in localStorage
+const lastVisit = localStorage.getItem("lastvisit");
+//?** deternine number of days (lastVisit - Date.now() / millisecondsToDays)  .toFixed(0)
+//?** display to whenever I want to
+//?** need update the stored date     localStorage.setItem('lastvisit', Date.now());
