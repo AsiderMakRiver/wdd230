ec51b05a14bcd55c67501bc0a7f050076d188cad
asidermakriver <asidermakriver@gmail.com>
2021-11-27 19:24:21 +0300

    Fix L11

--- lesson11/js/weather_api.js
+++ lesson11/js/weather_api.js
@@ -7,7 +7,8 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const towns = jsonObject["towns"];
     //function test(towns){
-    for (let i = 1; i < 4; i++) {
+    for (let i = 0; i < towns.length; i++) {
+      if (i !== 6 && i !== 0 && i !== 2) continue;
       // The order which I want! 6, 0, 2.
       //****************************************************Declare Variables*************
       let sectionHTML = document.createElement("section");
@@ -20,13 +21,15 @@ fetch(requestURL)
       let townAvRainFall = document.createElement("p");
       let figureHTML = document.createElement("figure");
       let imageHTML = document.createElement("img");
+      let figcaption = document.createElement("figcaption");
+      // let altForImg = "picture of city";
       //****************************************************Assign Variables***************
       townName.textContent = towns[i].name;
       townMotto.textContent = towns[i].motto;
       townFound.textContent = "Year Founded: " + towns[i].yearFounded;
       townCurPopulation.textContent = "Population: " + towns[i].currentPopulation;
       townAvRainFall.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-      imageHTML.setAttribute("src", towns[i].photo);
+      imageHTML.setAttribute("src", `images/${towns[i].photo}`);
       // *******************************************Push Variables Into the HTML Code******
       sectionHTML.appendChild(articleHTML);
       articleHTML.appendChild(townName);
@@ -37,8 +40,8 @@ fetch(requestURL)
       asideHTML.appendChild(townAvRainFall);
       sectionHTML.appendChild(figureHTML);
       figureHTML.appendChild(imageHTML);
-      document.querySelector("div.homeInfo").appendChild(sectionHTML);
+      figureHTML.appendChild(figcaption).classList.add("alt");
+      document.querySelector("div.homeInfo").appendChild(sectionHTML).classList.add("homeInfoSection");
+      // document.querySelector("figcapture.alt").appendChild(altForImg);
     }
-    //}
-    //test(towns[6]);
   });
--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -7,6 +7,7 @@ const options = {
   year: "numeric",
 };
 document.getElementById("lastupdated").textContent = a.toLocaleDateString("en-US", options);
+
 //TODO: **          The nav hamburger script
 function toggleMenu() {
   document.getElementById("primaryNav").classList.toggle("hide");
@@ -21,7 +22,7 @@ const navLinks = document.querySelectorAll("nav a").forEach((link) => {
 });
 
 //TODO: **            Local Storage
-const millisecondsToday = new Date().getTime(); //Milliseconds today from 1 january 1970
+/*const millisecondsToday = new Date().getTime(); //Milliseconds today from 1 january 1970
 localStorage.setItem("millisecondsTime", millisecondsToday); //Put that information to Local Storage
 const box1 = localStorage.getItem("milisecondsTime", millisecondsToday); //Get information from Local Storage to JS
 const millisecondsOneDay = 86400000;
@@ -29,6 +30,8 @@ localStorage.setItem("millisecondsOneDay", 86400000);
 const box2 = millisecondsToday;
 console.log(box1);
 console.log(box2);
+*/
+
 // localStorage.setItem("person", JSON.stringify(object));
 // console.log(localStorage.setItem("person", JSON.stringify(object)));
 
@@ -46,3 +49,21 @@ const lastVisit = localStorage.getItem("lastvisit");
 //?** deternine number of days (lastVisit - Date.now() / millisecondsToDays)  .toFixed(0)
 //?** display to whenever I want to
 //?** need update the stored date     localStorage.setItem('lastvisit', Date.now());
+
+//*************************************! Local Storage
+/* Calculates and alerts the user on how many days since their
+last visit to the Gallery or welcomes them if this is their initial vist.
+*/
+let savedDate = new Date(localStorage.getItem("savedDate"));
+const oneDay = 1000 * 60 * 60 * 24;
+let difTime = d.getTime() - savedDate.getTime();
+let difDays = Math.round(difTime / oneDay);
+if ("savedDate" in localStorage) {
+  document.getElementById("visitext").innerHTML = "Welcome Back - your last visit was " + difDays + " days ago.";
+} else {
+  document.getElementById("visitext").innerHTML = "Welcome to our Gallery";
+}
+saveDate();
+function saveDate() {
+  localStorage.setItem("savedDate", d);
+}
