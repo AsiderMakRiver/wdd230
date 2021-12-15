bee96a159b344a2babcebca0d384b48df506a15d
asidermakriver <asidermakriver@gmail.com>
2021-12-16 00:56:40 +0300

    Fix L14

--- lesson14/js/directoryApi.js
+++ lesson14/js/directoryApi.js
@@ -1,14 +1,13 @@
-const requestURL = $.getJSON("listOfCompanies.json");
+const requestURL = "https://asidermakriver.github.io/wdd230/lesson14/json/listOfCompanies.json";
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    // console.table(jsonObject); // temporary checking for valid response and data parsing
-    const towns = jsonObject["towns"];
-    //function test(towns){
-    for (let i = 0; i < towns.length; i++) {
-      if (i !== 6 && i !== 0 && i !== 2) continue;
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const localBusiness = jsonObject["localBusiness"];
+    for (let i = 0; i < localBusiness.length; i++) {
+      // if (i !== 6 && i !== 0 && i !== 2) continue;
       // The order which I want! 6, 0, 2.
       //****************************************************Declare Variables*************
       let sectionHTML = document.createElement("section");
@@ -24,12 +23,12 @@ fetch(requestURL)
       let figcaption = document.createElement("figcaption");
       // let altForImg = "picture of city";
       //****************************************************Assign Variables***************
-      townName.textContent = towns[i].name;
-      townMotto.textContent = towns[i].motto;
-      townFound.textContent = "Year Founded: " + towns[i].yearFounded;
-      townCurPopulation.textContent = "Population: " + towns[i].currentPopulation;
-      townAvRainFall.textContent = "Annual Rain Fall: " + towns[i].averageRainfall;
-      imageHTML.setAttribute("src", `images/${towns[i].photo}`);
+      townName.textContent = localBusiness[i].name;
+      townMotto.textContent = localBusiness[i].motto;
+      townFound.textContent = "Year Founded: " + localBusiness[i].yearFounded;
+      townCurPopulation.textContent = "Population: " + localBusiness[i].currentPopulation;
+      townAvRainFall.textContent = "Annual Rain Fall: " + localBusiness[i].averageRainfall;
+      imageHTML.setAttribute("src", `images/${localBusiness[i].photo}`);
       // *******************************************Push Variables Into the HTML Code******
       sectionHTML.appendChild(articleHTML);
       articleHTML.appendChild(townName);
