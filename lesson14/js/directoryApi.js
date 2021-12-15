583b0827d8b9ea4aca25f1714fbddf4c55e62184
asidermakriver <asidermakriver@gmail.com>
2021-12-16 01:40:02 +0300

    FinishL14

--- lesson14/js/directoryApi.js
+++ lesson14/js/directoryApi.js
@@ -7,40 +7,29 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const localBusiness = jsonObject["localBusiness"];
     for (let i = 0; i < localBusiness.length; i++) {
-      // if (i !== 6 && i !== 0 && i !== 2) continue;
-      // The order which I want! 6, 0, 2.
       //****************************************************Declare Variables*************
       let sectionHTML = document.createElement("section");
       let articleHTML = document.createElement("article");
-      let townName = document.createElement("h2");
-      let townMotto = document.createElement("p");
-      let asideHTML = document.createElement("aside");
-      let townFound = document.createElement("p");
-      let townCurPopulation = document.createElement("p");
-      let townAvRainFall = document.createElement("p");
+      let businessName = document.createElement("h2");
       let figureHTML = document.createElement("figure");
       let imageHTML = document.createElement("img");
-      let figcaption = document.createElement("figcaption");
-      // let altForImg = "picture of city";
+      let figcaptionHTML = document.createElement("figcaption");
+      let aHTML = document.createElement("a");
+      let textToAdd = document.createTextNode("Visit");
+      let textToAdd2 = document.createTextNode("Logo");
       //****************************************************Assign Variables***************
-      townName.textContent = localBusiness[i].name;
-      // townMotto.textContent = localBusiness[i].motto;
-      // townFound.textContent = "Year Founded: " + localBusiness[i].yearFounded;
-      // townCurPopulation.textContent = "Population: " + localBusiness[i].currentPopulation;
-      // townAvRainFall.textContent = "Annual Rain Fall: " + localBusiness[i].averageRainfall;
+      businessName.textContent = localBusiness[i].name;
       imageHTML.setAttribute("src", `images/${localBusiness[i].photo}`);
+      aHTML.setAttribute("href", localBusiness[i].website);
+      aHTML.setAttribute("target", `_blank`);
+      figcaptionHTML.setAttribute("alt", `logo`);
       // *******************************************Push Variables Into the HTML Code******
       sectionHTML.appendChild(articleHTML);
-      articleHTML.appendChild(townName);
-      articleHTML.appendChild(townMotto);
-      sectionHTML.appendChild(asideHTML);
-      asideHTML.appendChild(townFound);
-      asideHTML.appendChild(townCurPopulation);
-      asideHTML.appendChild(townAvRainFall);
+      articleHTML.appendChild(businessName);
       sectionHTML.appendChild(figureHTML);
       figureHTML.appendChild(imageHTML);
-      figureHTML.appendChild(figcaption).classList.add("alt");
+      figureHTML.appendChild(figcaptionHTML).appendChild(textToAdd2);
+      sectionHTML.appendChild(aHTML).appendChild(textToAdd);
       document.querySelector("div.homeInfo").appendChild(sectionHTML).classList.add("homeInfoSection");
-      // document.querySelector("figcapture.alt").appendChild(altForImg);
     }
   });
